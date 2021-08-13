186a867f6685b9b117c8511ed396b9d4e2e4b9f5
anderslemke <mail@anderslemke.dk>
2021-08-13 14:08:45 +0200

    Vary text size

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -11,11 +11,19 @@ export function getHtml(parsedReq: ParsedRequest) {
     let showText = text;
 
     if(text == "") {
-        showText= `Jeg vil gerne give dig muligheden for at prøve Zetland i en måned – uden binding. Og prisen? Den bestemmer du helt selv.`
+        showText = `Jeg vil gerne give dig muligheden for at prøve Zetland i en måned – uden binding. Og prisen? Den bestemmer du helt selv.`
     }
 
+    let textSize;
+    const length = showText.length;
+    if(length <= 20) { textSize = "text-6xl text-center pl-12" ;}
+    else if(length <= 42) { textSize = "text-5xl text-center pl-12" ;}
+    else if(length <= 70) { textSize = "text-4xl text-center pl-12" ;}
+    else { textSize = "pl-16 text-3xl" };
+
     const quoteClasses = "text-6xl italic text-gray-400";
-    showText = `<span class="quote quote--start ${quoteClasses} block absolute">“</span>${showText}<span class="qoute quote--end ${quoteClasses} relative inline-block"><span class="absolute mt-5 block">”</span></span>`;
+    const textWithQuotes = `<span class="quote quote--start ${quoteClasses} relative inline-block"><span class="absolute block -mt-14 -ml-8">“</span></span>${showText}<span class="qoute quote--end ${quoteClasses} relative inline-block"><span class="absolute mt-5 block">”</span></span>`;
+
 
     return `<!DOCTYPE html>
 <html>
@@ -28,7 +36,7 @@ export function getHtml(parsedReq: ParsedRequest) {
             orientation: portrait;
             font-family: 'Italian Plate No2','Gotham SSm A','Gotham SSm B',Arial,Verdana,sans-serif;
             margin: 0;
-            font-size: 32px;
+            font-size: 30px;
         }
         body {
             background-color: #FAF8F0;
@@ -49,28 +57,24 @@ export function getHtml(parsedReq: ParsedRequest) {
         .qoute {
             line-height: 0;
         }
-        .quote--start {
-            margin-left: -0.42em;
-            margin-top: -0.25em;
-        }
     </style>
     <body>
         <div class="h-screen flex">
             <div class="m-3 p-3 border-orange flex-grow flex items-center justify-start">
-                <div class="flex flex-col justify-between h-full">
-                    <div class="relative text-3xl text-gray-800 font-bold leading-tight flex-grow flex items-center">
-                        <div class="pl-16 pr-12 ml-1">
+                <div class="flex flex-col justify-between h-full w-full">
+                    <div class="relative ${textSize} text-gray-800 font-bold leading-tight flex-grow flex items-center justify-center w-full">
+                        <div class="pr-12 ml-1">
                             ${emojify(
-                                marked(showText)
+                                marked(textWithQuotes)
                             )}
                         </div>
                     </div>
                     <div class="flex justify-between items-end">
                         <div class="flex justify-start items-end">
                             ${ getImage(image) }
-                            <p class="pl-3 text-base leading-none text-gray-500 font-extralight">
-                                ${ name }<br />
-                                <span class="text-sm italic text-gray-400">Medlem af Zetland</span>
+                            <p class="pl-3 text-base leading-none text-gray-500 mb-1">
+                                <span class="font-normal">${ name }${ length }${textSize}</span><br />
+                                <span class="text-sm font-extralight italic ">Medlem af Zetland</span>
                             </p>
                         </div>
                         <img class="h-5" src="https://pdf.zetland.dk/gift_card_images/medium-logo-orange.png" />
