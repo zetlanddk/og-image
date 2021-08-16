8e96975e9d48607c94f1351d2e61456df1723938
anderslemke <mail@anderslemke.dk>
2021-08-16 13:35:03 +0200

    No text with image

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -7,24 +7,34 @@ const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
 export function getHtml(parsedReq: ParsedRequest) {
-    const { text, name, image } = parsedReq;
-    let showText = text;
+    const { text, name, image, firstName } = parsedReq;
+    let showText, isQoute;
 
     const debug = false;
 
+    let textSize;
+
     if(text == "") {
-        showText = `Jeg vil gerne give dig muligheden for at prøve Zetland i en måned – uden binding. Og prisen? Den bestemmer du helt selv.`
+        isQoute = false;
+
+        showText = `${firstName} vil gerne give dig muligheden for at prøve Zetland i en måned – uden binding. Og prisen? Den bestemmer du helt selv.`
+
+        textSize = 'text-2xl'
+    } else {
+        isQoute = true;
+
+        const length = text.length;
+        if(length <= 15) { textSize = "text-6xl text-center pl-14" ;}
+        else if(length <= 39) { textSize = "text-5xl text-center pl-14" ;}
+        else if(length <= 70) { textSize = "text-4xl pl-16" ;}
+        else { textSize = "pl-16 text-3xl"; }
+
+        const quoteClasses = "italic text-gray-400";
+        const textWithQuotes = `<span class="quote quote--start ${quoteClasses} relative inline-block"><span class="absolute block">“</span></span>${text}<span class="quote quote--end ${quoteClasses} relative inline-block"><span class="absolute block">”</span></span>`;
+
+        showText = textWithQuotes;
     }
 
-    let textSize;
-    const length = showText.length;
-    if(length <= 15) { textSize = "text-6xl text-center pl-14" ;}
-    else if(length <= 39) { textSize = "text-5xl text-center pl-14" ;}
-    else if(length <= 70) { textSize = "text-4xl pl-16" ;}
-    else { textSize = "pl-16 text-3xl" };
-
-    const quoteClasses = "italic text-gray-400";
-    const textWithQuotes = `<span class="quote quote--start ${quoteClasses} relative inline-block"><span class="absolute block">“</span></span>${showText}<span class="quote quote--end ${quoteClasses} relative inline-block"><span class="absolute block">”</span></span>`;
 
 
     return `<!DOCTYPE html>
@@ -70,17 +80,20 @@ export function getHtml(parsedReq: ParsedRequest) {
         }
     </style>
     <body>
+        <img class="h-5 fixed bottom-6 right-6" src="https://pdf.zetland.dk/gift_card_images/medium-logo-orange.png" />
         <div class="h-screen flex">
             <div class="m-3 p-3 border-orange flex-grow flex items-center justify-start">
-                <div class="flex flex-col justify-between h-full w-full">
-                    <div class="relative ${textSize} text-gray-800 font-bold leading-tight flex-grow flex items-center justify-center w-full">
+                ${ isQoute ? "" : '<img class="h-72 pl-8 pr-8" src="https://height-files.storage.googleapis.com/5c0b5de0-689a-447c-9168-a7b523e4c97a.png" />' }
+
+                <div class="flex ${ isQoute ? "flex-col justify-between" : "flex-col-reverse justify-center" } h-full w-full">
+                    <div class="relative ${textSize} text-gray-800 ${ isQoute ? "font-bold flex-grow mt-2" : "" } leading-tight flex items-center justify-center w-full">
                         <div class="pr-14 ml-1">
                             ${emojify(
-                                marked(textWithQuotes)
+                                marked(showText)
                             )}
                         </div>
                     </div>
-                    <div class="flex justify-between items-end">
+                    <div class="flex justify-between items-end ${isQoute ? "" : "mb-6"}">
                         <div class="flex justify-start items-end">
                             ${ getImage(image) }
                             <p class="pl-3 text-base leading-none text-gray-500 mb-1">
@@ -88,7 +101,6 @@ export function getHtml(parsedReq: ParsedRequest) {
                                 <span class="text-sm font-extralight italic ">Medlem af Zetland</span>
                             </p>
                         </div>
-                        <img class="h-5" src="https://pdf.zetland.dk/gift_card_images/medium-logo-orange.png" />
                     </div>
                 </div>
             </div>
