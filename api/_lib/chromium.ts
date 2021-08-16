949997e410c61931c98890294d2f6b4fb5bb1067
anderslemke <mail@anderslemke.dk>
2021-08-16 12:00:01 +0200

    Better jpegs

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -17,6 +17,7 @@ export async function getScreenshot(html: string, type: FileType, isDev: boolean
     const page = await getPage(isDev);
     await page.setViewport({ width: 1200, height: 630, deviceScaleFactor: 1 });
     await page.setContent(html, { waitUntil: 'networkidle0' });
-    const file = await page.screenshot({ type });
+    const options = type == 'png' ? { type } : { type, quality: 100 };
+    const file = await page.screenshot(options);
     return file;
 }
